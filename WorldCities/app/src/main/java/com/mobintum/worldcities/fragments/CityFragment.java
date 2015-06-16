ada890e9cda4fcbeab6edb42b2a2c432db51da4e
rickstart <ricardo.celj@gmail.com>
2015-06-15 19:45:46 -0500

    Complete World Cities

--- WorldCities/app/src/main/java/com/mobintum/worldcities/fragments/CityFragment.java
+++ WorldCities/app/src/main/java/com/mobintum/worldcities/fragments/CityFragment.java
@@ -5,13 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.mobintum.worldcities.R;
 import com.mobintum.worldcities.models.City;
+import com.squareup.picasso.Picasso;
 
 public class CityFragment extends Fragment {
 
@@ -47,6 +51,17 @@ public class CityFragment extends Fragment {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         gMap = mapFragment.getMap();
         gMap.addMarker(new MarkerOptions().position(new LatLng(city.getLatitude(),city.getLongitude())).title(city.getName()));
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(city.getLatitude(),city.getLongitude()),10));
+        gMap.setMyLocationEnabled(true);
+        gMap.setBuildingsEnabled(true);
+
+        TextView txtNameCity = (TextView) view.findViewById(R.id.txtNameCity);
+        txtNameCity.setText(city.getName());
+
+        ImageView imgCity = (ImageView) view.findViewById(R.id.imgCity);
+
+        Picasso.with(getActivity()).load(city.getPicUrl()).into(imgCity);
+
 
         return view;
     }
